f0db05657197341515f96f2dc9851e1aa4eaaedb
anashmamouch <anashmamouch90@gmail.com>
2015-06-21 16:50:10 +0000

    final commit :tada:

--- Gemfile
+++ Gemfile
@@ -1,15 +1,21 @@
 source 'https://rubygems.org'
-
+ruby "2.2.1"
 
 gem 'rails', '4.2.1'
 gem 'sqlite3'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
-gem 'haml', '~> 4.0.6'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
+
+gem 'haml', '~> 4.0.6'
+gem 'simple_form', '~> 3.1.0'
+gem 'bootstrap-sass', '~> 3.3.5'
+gem 'paperclip', '~> 4.3.0'
+gem 'cocoon', '~> 1.2.6'
+
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 group :development, :test do
@@ -18,3 +24,8 @@ group :development, :test do
   gem 'spring'
 end
 
+group :production do
+  gem 'pg',             '0.18.2'
+  gem 'rails_12factor', '0.0.2'
+  gem 'puma',           '2.11.1'
+end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,15 @@
 class RecipesController < ApplicationController
   before_action :find_recipe, only: [:show, :edit, :update, :destroy] 
+	
 	def index
-		
+		@recipes = Recipe.all.order("created_at DESC")
 	end
 
 	def show
-		
+		respond_to do |format|
+			format.html
+			format.json {render json: @recipe, location: @recipe}
+		end
 	end
 
 	def new
@@ -14,23 +18,49 @@ class RecipesController < ApplicationController
 
 	def create
 		@recipe = Recipe.new(recipe_params)
+
+		respond_to do |format|
+			if @recipe.save
+			  format.html { redirect_to @recipe, notice: "Successfully created new recipe"}
+			  format.json { render json: @recipe, status: :created,  location: @recipe}
+			else
+		   	   format.html {render 'new'}
+			end
+		end
+
+	end
+
+	def edit 
+		
 	end
 
 	def update
+		respond_to do |format|
+			if @recipe.update(recipe_params)
+				format.html {redirect_to @recipe}
+				format.json { render json: @recipe, status: :updated,  location: @recipe}
+			else 
+				format.html {render 'edit'}
+			end
+		end
 		
 	end
 
 	def destroy
-		
+		@recipe.destroy 
+		redirect_to root_path, notice: "Successfully deleted recipe"
 	end
 
 	private 
 		def find_recipe
-			@recipe = Recipes.find(params[:id])
+			@recipe = Recipe.find(params[:id])
 		end
 
 		def recipe_params
-			params.require(:recipe).permit(:title, :description)
+			params.require(:recipe).permit(:title, :description, :image, 
+										ingredients_attributes: [:id, :name, :_destroy], 
+										directions_attributes: [:id, :step, :_destroy])
+
 		end
 
 end
